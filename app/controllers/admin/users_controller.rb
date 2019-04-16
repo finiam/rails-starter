005dea67a574ddc830b17183de49360b83f6fbda
José Sousa <josesousa9000@gmail.com>
2019-04-16 22:10:18 +0100

    Ignore empty password

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,26 @@
 module Admin
   class UsersController < Admin::ApplicationController
-    # To customize the behavior of this controller,
-    # you can overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = User.
-    #     page(params[:page]).
-    #     per(10)
-    # end
+    def update
+      if requested_resource.update(filtered_resource_params)
+        redirect_to(
+          [namespace, requested_resource],
+          notice: translate_with_resource("update.success"),
+        )
+      else
+        render :edit, locals: {
+          page: Administrate::Page::Form.new(dashboard, requested_resource),
+        }
+      end
+    end
 
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   User.find_by!(slug: param)
-    # end
+    private
 
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
+    def filtered_resource_params
+      custom_resource_params = resource_params
+
+      custom_resource_params.delete("password") if custom_resource_params["password"] == ""
+
+      custom_resource_params
+    end
   end
 end
