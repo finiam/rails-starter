57473e3539ce13e41d8f1a712cc17253213e6136
José Sousa <josesousa9000@gmail.com>
2020-02-06 17:27:23 +0000

    wip

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -2,12 +2,5 @@ const path = require("path")
 
 module.exports = {
   test: /\.js|.jsx(\.erb)?$/,
-  exclude:
-    process.env.NODE_ENV === "development"
-      ? // watch everything except node_modules
-        [/node_modules/]
-      : // exclude stories and tests from other environments
-        [/node_modules/, /.+\.stories\.js$/, /.+\.test\.js/],
-  include: [path.join(__dirname, "../../../frontend") ,/node_modules\/wouter/],
   loader: "babel-loader"
 };
