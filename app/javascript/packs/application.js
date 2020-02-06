c8048894912e9b0b193aefe93103ad2d6bef205a
José Sousa <josesousa9000@gmail.com>
2020-02-06 18:12:23 +0000

    wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,1 @@
-import "@babel/polyfill"
-
 require("../../../frontend/index");
--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -1,10 +1,5 @@
-const dependenciesToTranspile = ["wouter"];
-const nodeModules = new RegExp(
-  `node_modules/(?!(${dependenciesToTranspile.join("|")})/).*`
-);
-
 module.exports = {
   test: /\.js|.jsx(\.erb)?$/,
-  exclude: [nodeModules, /.+\.stories\.js$/, /.+\.test\.js/],
+  exclude: [/node_modules/, /.+\.stories\.js$/, /.+\.test\.js/],
   loader: "babel-loader"
 };
