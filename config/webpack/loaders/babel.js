31915d1fa09f2bd4136fce15856f27bb36c37c00
José Sousa <josesousa9000@gmail.com>
2020-02-06 18:45:35 +0000

    wip

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -1,5 +1,10 @@
+const dependenciesToTranspile = ["wouter"];
+const nodeModules = new RegExp(
+  `node_modules/(?!(${dependenciesToTranspile.join("|")})/).*`
+);
+
 module.exports = {
   test: /\.js|.jsx(\.erb)?$/,
-  exclude: [/node_modules/, /.+\.stories\.js$/, /.+\.test\.js/],
+  exclude: [nodeModules, /.+\.stories\.js$/, /.+\.test\.js/],
   loader: "babel-loader"
 };
