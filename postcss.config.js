e2abf09e439dbf2c8519018882a700f86b40ae5f
Francisco Sousa <josesousa9000@gmail.com>
2020-02-15 18:53:11 +0000

    Update readme and docs

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,20 @@
 /* eslint-disable global-require */
 
+/*
+  The order of the plugins in this file is very important.
+
+  postcss-import should be at beggining so @import statements
+  get processed first.
+
+  cssnano should always be at the bottom, as it's the last thing
+  that should run over our CSS
+
+  The rest is hit or miss, check each plugin documentation
+  for potential interactions with other plugins
+*/
+
 module.exports = {
   plugins: [
-    process.env.NODE_ENV === "production"
-      ? require("cssnano")({
-          preset: "default"
-        })
-      : null,
     require("postcss-import"),
     require("precss"),
     require("postcss-preset-env")({
@@ -16,6 +24,11 @@ module.exports = {
       stage: 3
     }),
     require("postcss-color-function"),
-    require("postcss-flexbugs-fixes")
+    require("postcss-flexbugs-fixes"),
+    process.env.NODE_ENV === "production"
+      ? require("cssnano")({
+          preset: "default"
+        })
+      : null
   ]
 };
