ac7f14c7ddc562a809e25a6ad0d398a6db7581b0
Francisco Sousa <josesousa9000@gmail.com>
2020-03-05 11:53:58 +0000

    Chores: dockerfile, webpack and ruby version
    Make some quality of life fixes to the dockerfile. Remove optimization params of Webpack as we don't need them. Update ruby and javascript versions.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,7 +6,6 @@ const { sync } = require("glob");
 const path = require("path");
 
 const loadersDir = join(__dirname, "loaders");
-const notServerRendering = name => name !== "server_rendering";
 const sharedConfig = {
   stats: {
     children: false
@@ -15,23 +14,6 @@ const sharedConfig = {
   module: {
     // Import loaders from our loaders folder
     rules: sync(join(loadersDir, "*.js")).map(loader => require(loader))
-  },
-
-  // Never split the chunks from the SSR pack
-  // we need that whole
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          chunks(chunk) {
-            return notServerRendering(chunk.name);
-          },
-          name: "vendors",
-          priority: 1
-        }
-      }
-    }
   },
 
   resolve: {
