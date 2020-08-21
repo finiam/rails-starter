d4291f520fed7a41c5a7bfda3a4c7796b85666f3
José Sousa <josesousa9000@gmail.com>
2020-08-21 15:39:47 +0100

    Remove custom fetch wrapper and use redaxios

--- frontend/api/auth.js
+++ frontend/api/auth.js
@@ -1,22 +1,19 @@
-import easyFetch from "root/shared/easyFetch";
+import redaxios from "redaxios";
 
 export async function getSession() {
-  const response = await easyFetch("/session");
+  const response = await redaxios.get("/session");
 
   return response;
 }
 
 export async function login(email, password) {
-  const response = await easyFetch("/session", {
-    method: "POST",
-    body: { email, password },
-  });
+  const response = await redaxios.post("/session", { email, password });
 
   return response;
 }
 
 export async function logout() {
-  const response = await easyFetch("/session", { method: "DELETE" });
+  const response = await redaxios.delete("/session");
 
   return response;
 }
