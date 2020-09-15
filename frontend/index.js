a13e7c4b8133e1eb176576e31073132baae5cf6d
Francisco Sousa <francisco.sousa@hey.com>
2020-09-15 17:03:04 +0100

    Fix CSRF token integration

--- frontend/index.js
+++ frontend/index.js
@@ -4,9 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "root/components/App";
+import setupCsrfToken from "root/api/setupCsrfToken";
 
 import "./styles/reset.css";
 
 window.addEventListener("DOMContentLoaded", () => {
+  setupCsrfToken();
+
   ReactDOM.render(<App />, document.getElementById("root"));
 });
