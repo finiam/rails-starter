d9ed640bad559fb158225f02f8a01c9d0b9eeee7
Francisco Sousa <josesousa9000@gmail.com>
2020-10-23 11:56:01 +0100

    Update frontend packages and fix storybook css (#597)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ["../frontend/**/*.stories.js"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-css-modules-preset",
+  ],
 };
