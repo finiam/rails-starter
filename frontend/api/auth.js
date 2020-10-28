8d7adf210489aa5d8c93757ab68ed22be8d94e98
Tânia Rocha <taniapassosrocha@gmail.com>
2020-10-28 12:18:54 +0000

    Gerenate a new token on logout

--- frontend/api/auth.js
+++ frontend/api/auth.js
@@ -1,4 +1,5 @@
 import redaxios from "redaxios";
+import setupCsrfToken from "root/api/setupCsrfToken";
 
 export async function getSession() {
   const response = await redaxios.get("/session");
@@ -14,6 +15,8 @@ export async function login(email, password) {
 
 export async function logout() {
   const response = await redaxios.delete("/session");
+
+  setupCsrfToken(response.data);
 
   return response;
 }
--- frontend/api/setupCsrfToken.js
+++ frontend/api/setupCsrfToken.js
@@ -1,7 +1,8 @@
 import redaxios from "redaxios";
 
-export default function setupCsrfToken() {
-  const csrfToken = document.querySelector("meta[name=csrf-token]").content;
+export default function setupCsrfToken(token) {
+  const csrfToken =
+    token || document.querySelector("meta[name=csrf-token]").content;
 
   redaxios.defaults.headers = { "X-CSRF-Token": csrfToken };
 }
