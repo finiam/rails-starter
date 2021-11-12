1ff43151972d4ed708e628e319f5de6c72c1e7eb
Francisco Sousa <francisco.sousa@hey.com>
2021-11-12 20:23:06 +0000

    Regenerate CSRF token on each request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  around_action :attach_csrf_token_to_header
 
   private
 
+  # SORCERY OVERRIDE
+  #
+  # Respond with a not found response when not authenticated
   def not_authenticated
     head :not_found
+  end
+
+  # SORCERY OVERRIDE
+  #
+  # Set the response after the yield of a controller action
+  # so that each request always has the csrf token on the
+  # headers of the response
+  def attach_csrf_token_to_header
+    yield
+
+    token = session[:_csrf_token] || form_authenticity_token
+    response.headers["X-CSRF-Token"] = token
   end
 end
--- frontend/api/setupCsrfToken.js
+++
@@ -1,7 +0,0 @@
-import redaxios from "redaxios";
-
-export default function setupCsrfToken() {
-  const csrfToken = document.querySelector("meta[name=csrf-token]").content;
-
-  redaxios.defaults.headers = { "X-CSRF-Token": csrfToken };
-}
